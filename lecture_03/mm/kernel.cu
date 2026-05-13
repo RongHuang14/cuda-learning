74deb6c4ecae99047d24e881dccf77957081b408
root <root@ubuntu22.04>
2026-05-14 06:46:04 +0800

    lec05:mm tile

--- lecture_03/mm/kernel.cu
+++ lecture_03/mm/kernel.cu
@@ -1,15 +1,28 @@
 #include "common.h"
 #include "timer.h"
+#define TILE_DIM 32
 
 // 每个线程计算 C 中的一个元素 C[row][col] = A[row] · B[:,col]
-__global__ void mm_kernel(float* A, float* B, float* C, unsigned int N) {
+__global__ void mm_tiled_kernel(float* A, float* B, float* C, unsigned int N) {
     unsigned int row = blockIdx.y*blockDim.y + threadIdx.y;
     unsigned int col = blockIdx.x*blockDim.x + threadIdx.x;
 
-    float sum = 0.0f;
-    for (unsigned int i = 0; i < N; ++i)
-        sum += A[row*N + i] * B[i*N + col];  // A 的第 row 行 · B 的第 col 列
+    __shared__ float A_s[TILE_DIM][TILE_DIM];
+    __shared__ float B_s[TILE_DIM][TILE_DIM];
+
+    float sum = 0.0f;    
+    for (unsigned int tile = 0; tile < N/TILE_DIM; ++tile) {
+    	A_s[threadIdx.y][threadIdx.x] = A[row*N + tile*TILE_DIM + threadIdx.x];
+	B_s[threadIdx.y][threadIdx.x] = B[(tile*TILE_DIM + threadIdx.y)*N + col];
+	__syncthreads();
+
+	for (unsigned int i = 0; i < TILE_DIM; ++i) {
+		sum += A_s[threadIdx.y][i]*B_s[i][threadIdx.x];
+	}
+	__syncthreads();
+    }
     C[row*N + col] = sum;
+
 }
 
 void mm_gpu(float* A, float* B, float* C, unsigned int N) {
@@ -35,7 +48,7 @@ void mm_gpu(float* A, float* B, float* C, unsigned int N) {
     dim3 numThreadsPerBlock(32, 32);
     dim3 numBlocks((N + numThreadsPerBlock.x - 1)/numThreadsPerBlock.x,
                    (N + numThreadsPerBlock.y - 1)/numThreadsPerBlock.y);
-    mm_kernel<<< numBlocks, numThreadsPerBlock >>>(A_d, B_d, C_d, N);
+    mm_tiled_kernel<<< numBlocks, numThreadsPerBlock >>>(A_d, B_d, C_d, N);
     cudaDeviceSynchronize();
     stopTime(&timer);
     printElapsedTime(timer, "Kernel time", GREEN);
